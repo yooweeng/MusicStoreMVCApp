fe8ab10497a8c182cc2aad464ba97b7485c847b1
yooweeng <nyw20001@hotmail.com>
2023-05-08 12:17:59 +0800

    add identity settings to allow int type for id and add default roles

--- MusicStoreMVCApp/App_Start/IdentityConfig.cs
+++ MusicStoreMVCApp/App_Start/IdentityConfig.cs
@@ -106,4 +106,17 @@ namespace MusicStoreMVCApp
             return new ApplicationSignInManager(context.GetUserManager<ApplicationUserManager>(), context.Authentication);
         }
     }
+
+    public class ApplicationRoleManager : RoleManager<ApplicationRole, long>
+    {
+        public ApplicationRoleManager(RoleStore<ApplicationRole, long, ApplicationUserRole> store) : base(store)
+        {
+        }
+
+        public static ApplicationRoleManager Create(IdentityFactoryOptions<ApplicationRoleManager> options, IOwinContext context)
+        {
+            var a = new RoleStore<ApplicationRole, long, ApplicationUserRole>(context.Get<ApplicationDbContext>());
+            return new ApplicationRoleManager(a);
+        }
+    }
 }
--- MusicStoreMVCApp/Models/IdentityModels.cs
+++ MusicStoreMVCApp/Models/IdentityModels.cs
@@ -1,14 +1,25 @@
 ﻿using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using System.Web;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
 
 namespace MusicStoreMVCApp.Models
 {
+    public enum UserType
+    {
+        Admin = 1,
+        Customer = 2,
+        Seller = 3
+    }
+
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public UserType UserType { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
@@ -18,6 +29,11 @@ namespace MusicStoreMVCApp.Models
         }
     }
 
+    public class ApplicationUserClaim : IdentityUserClaim<long> { }
+    public class ApplicationUserLogin : IdentityUserLogin<long> { }
+    public class ApplicationUserRole : IdentityUserRole<long> { }
+    public class ApplicationRole : IdentityRole<long, ApplicationUserRole> { }
+
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
@@ -25,9 +41,32 @@ namespace MusicStoreMVCApp.Models
         {
         }
 
+        static ApplicationDbContext()
+        {
+            Database.SetInitializer<ApplicationDbContext>(new ApplicationDBInitializer());
+        }
+
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+    }
+
+    public class ApplicationDBInitializer : CreateDatabaseIfNotExists<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            ConfigureDefaultRolesAndUsers();
+            base.Seed(context);
+        }
+
+        private void ConfigureDefaultRolesAndUsers()
+        {
+            var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
+
+            if (!roleManager.RoleExists("Admin")) roleManager.Create(new ApplicationRole() { Name = "Admin" });
+            if (!roleManager.RoleExists("Customer")) roleManager.Create(new ApplicationRole() { Name = "Customer" });
+            if (!roleManager.RoleExists("Seller")) roleManager.Create(new ApplicationRole() { Name = "Seller" });
         }
     }
 }
