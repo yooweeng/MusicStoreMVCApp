b090823311838b55569c0b60f55c238bed44c2f0
yooweeng <nyw20001@hotmail.com>
2023-05-09 12:53:49 +0800

    remove contact page

--- MusicStoreMVCApp/Controllers/HomeController.cs
+++ MusicStoreMVCApp/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace MusicStoreMVCApp.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
