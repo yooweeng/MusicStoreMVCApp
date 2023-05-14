25af9bdf6b56133f2de3032431bc174c70f6e20f
yooweeng <nyw20001@hotmail.com>
2023-05-15 00:12:02 +0800

    implement customer index page

--- MusicStoreMVCApp/Controllers/CustomerController.cs
+++ MusicStoreMVCApp/Controllers/CustomerController.cs
@@ -9,10 +9,31 @@ namespace MusicStoreMVCApp.Controllers
     [Authorize(Roles = "Customer")]
     public class CustomerController : Controller
     {
+        readonly MusicStoreAppEntities db = new MusicStoreAppEntities();
+
         // GET: Customer
         public ActionResult Index()
         {
+            List<Movie> movies = db.Movies.ToList();
+
+            return View(movies);
+        }
+
+        public ActionResult Cart()
+        {
             return View();
+        }
+
+        public ActionResult MovieDetail(int id)
+        {
+            Movie movieById = db.Movies.Where(movie => movie.Id == id).FirstOrDefault();
+
+            if (movieById != null)
+            {
+                return View(movieById);
+            }
+
+            return View("Error");
         }
     }
 }
