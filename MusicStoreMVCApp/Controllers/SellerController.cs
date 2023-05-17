b65b369c647867dabcd67af5d25bedb88a898bee
yooweeng <nyw20001@hotmail.com>
2023-05-17 17:08:42 +0800

    apply sorting on order based on order date then id

--- MusicStoreMVCApp/Controllers/SellerController.cs
+++ MusicStoreMVCApp/Controllers/SellerController.cs
@@ -113,7 +113,10 @@ namespace MusicStoreMVCApp.Controllers
             int currentUserId = int.Parse(User.Identity.GetUserId());
             int currentSellerId = db.Sellers.Where(seller => seller.UserId == currentUserId).Single().SellerId;
 
-            List<OrderMovie> orders = db.OrderMovies.Where(order => order.Movie.SellerId == currentSellerId).ToList();
+            List<OrderMovie> orders = db.OrderMovies.Where(order => order.Movie.SellerId == currentSellerId)
+                                                     .OrderByDescending(order => order.Order.Date)
+                                                     .ThenByDescending(order => order.Order.Id).ToList();
+
             return View(orders);
         }
 
