b911b2b48df48e573d7af6a3adad4e17afc59336
yooweeng <nyw20001@hotmail.com>
2023-05-17 20:12:49 +0800

    implement view and logic for home page

--- MusicStoreMVCApp/Controllers/HomeController.cs
+++ MusicStoreMVCApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MusicStoreMVCApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,32 @@ namespace MusicStoreMVCApp.Controllers
 {
     public class HomeController : Controller
     {
+        readonly MusicStoreAppEntities db = new MusicStoreAppEntities();
+
         public ActionResult Index()
         {
-            return View();
+            List<Movie> movies = db.Movies.ToList();
+            List<Genre> genres = db.Genres.ToList();
+            List<MovieGenre> movieGenres = db.MovieGenres.ToList();
+            List<MovieIdGenreIdModel> movieIdGenreIds = new List<MovieIdGenreIdModel>();
+
+            foreach (MovieGenre movieGenre in movieGenres)
+            {
+                movieIdGenreIds.Add(new MovieIdGenreIdModel()
+                {
+                    MovieId = movieGenre.MovieId,
+                    GenreId = movieGenre.GenreId
+                });
+            }
+
+            GuestCustomerIndexViewModel model = new GuestCustomerIndexViewModel()
+            {
+                Movies = movies,
+                Genres = genres,
+                MovieIdGenreIds = movieIdGenreIds
+            };
+
+            return View(model);
         }
     }
 }
